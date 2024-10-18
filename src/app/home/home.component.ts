ca0073618c3f4db3dd6f1fecb5576a4492cc5405
Srinath <srinathsaks@gmail.com>
2024-10-18 23:27:25 +0530

    image-fixing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit, OnDestroy{
-  
-  @ViewChild('footerSection', { static: false }) footerSection!: ElementRef;
+export class HomeComponent implements OnInit, OnDestroy {
 
+  @ViewChild('footerSection', { static: false }) footerSection!: ElementRef;
 
   images: string[] = [
     'assets/images/prestige1.jpg',
@@ -23,46 +22,59 @@ export class HomeComponent implements OnInit, OnDestroy{
   private imageIndex: number = 0;
   private intervalId: any;
   isLoading: boolean = true;
+  isBrowser: boolean = false;
 
-  constructor() { }
-
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    // Check if the code is running in the browser
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 
   ngOnInit(): void {
-    // Preload images to avoid rendering issues
-    this.preloadImages().then(() => {
-      this.currentImage = this.images[this.imageIndex];
-      this.startSlideshow();
-      this.isLoading = false; // Set loading to false once images are loaded
-    });
+    if (this.isBrowser) {
+      // Preload images to avoid rendering issues in browser only
+      this.preloadImages().then(() => {
+        this.currentImage = this.images[this.imageIndex];
+        this.startSlideshow();
+        this.isLoading = false; // Set loading to false once images are loaded
+      });
+    }
   }
 
   preloadImages(): Promise<void> {
     const imagePromises = this.images.map(image => {
       return new Promise<void>((resolve) => {
-        const img = new Image();
-        img.src = image;
-        img.onload = () => resolve(); // Resolve promise once image is loaded
+        if (this.isBrowser) {
+          const img = new Image(); // Use Image constructor only in the browser
+          img.src = image;
+          img.onload = () => resolve(); 
+        } else {
+          resolve(); 
+        }
       });
     });
-    return Promise.all(imagePromises).then(() => {}); // Wait for all images to load
+    return Promise.all(imagePromises).then(() => { });
   }
 
   startSlideshow(): void {
-    this.intervalId = setInterval(() => {
-      this.imageIndex = (this.imageIndex + 1) % this.images.length;
-      this.currentImage = this.images[this.imageIndex];
-    }, 2000); // Change image every 2 seconds
+    if (this.isBrowser) {
+      this.intervalId = setInterval(() => {
+        this.imageIndex = (this.imageIndex + 1) % this.images.length;
+        this.currentImage = this.images[this.imageIndex];
+      }, 2000); 
+    }
   }
 
   scrollToFooter(): void {
-    if(this.footerSection)
-    this.footerSection.nativeElement.scrollIntoView({ behavior: 'smooth'});
+    if (this.footerSection && this.isBrowser) {
+      this.footerSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 
   ngOnDestroy(): void {
-    if (this.intervalId) {
+    if (this.intervalId && this.isBrowser) {
       clearInterval(this.intervalId); 
     }
   }
 
 }
+
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit, OnDestroy {
 
   @ViewChild('footerSection', { static: false }) footerSection!: ElementRef;
-
 
   images: string[] = [
     'assets/images/ramnuja2.jpg',
@@ -23,45 +22,58 @@ export class ProjectsComponent {
   private imageIndex: number = 0;
   private intervalId: any;
   isLoading: boolean = true;
+  isBrowser: boolean = false; // To track if the platform is browser
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId); // Check if running in browser
+  }
 
   ngOnInit(): void {
-    // Preload images to avoid rendering issues
-    this.preloadImages().then(() => {
-      this.currentImage = this.images[this.imageIndex];
-      this.startSlideshow();
-      this.isLoading = false; // Set loading to false once images are loaded
-    });
+    if (this.isBrowser) {
+      // Preload images only in the browser
+      this.preloadImages().then(() => {
+        this.currentImage = this.images[this.imageIndex];
+        this.startSlideshow();
+        this.isLoading = false; // Set loading to false once images are loaded
+      });
+    }
   }
 
   preloadImages(): Promise<void> {
     const imagePromises = this.images.map(image => {
       return new Promise<void>((resolve) => {
-        const img = new Image();
-        img.src = image;
-        img.onload = () => resolve(); 
+        if (this.isBrowser) {
+          const img = new Image(); // Use Image constructor only in the browser
+          img.src = image;
+          img.onload = () => resolve(); // Resolve promise once image is loaded
+        } else {
+          resolve(); // Resolve immediately in SSR (skip image preloading in server)
+        }
       });
     });
-    return Promise.all(imagePromises).then(() => {}); // Wait for all images to load
+    return Promise.all(imagePromises).then(() => { });
   }
 
   startSlideshow(): void {
-    this.intervalId = setInterval(() => {
-      this.imageIndex = (this.imageIndex + 1) % this.images.length;
-      this.currentImage = this.images[this.imageIndex];
-    }, 2000); // Change image every 2 seconds
+    if (this.isBrowser) {
+      this.intervalId = setInterval(() => {
+        this.imageIndex = (this.imageIndex + 1) % this.images.length;
+        this.currentImage = this.images[this.imageIndex];
+      }, 2000); // Change image every 2 seconds
+    }
   }
 
   scrollToFooter(): void {
-    if(this.footerSection)
-    this.footerSection.nativeElement.scrollIntoView({ behavior: 'smooth'});
+    if (this.footerSection && this.isBrowser) {
+      this.footerSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 
   ngOnDestroy(): void {
-    if (this.intervalId) {
-      clearInterval(this.intervalId); 
+    if (this.intervalId && this.isBrowser) {
+      clearInterval(this.intervalId); // Clear the interval when the component is destroyed
     }
   }
 
 }
+
